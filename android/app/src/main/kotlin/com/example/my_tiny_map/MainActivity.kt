22a7322fc91f4c86aabd2f6736780c2dfe1dbbb2
Dylan <93738662+baka9131@users.noreply.github.com>
2023-06-26 15:31:40 +0900

    Feat/cha (#9)
    * first

--- android/app/src/main/kotlin/com/example/my_tiny_map/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_tiny_map/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.my_tiny_map
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
 }
---
+++ lib/design/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final mainThemeDate = ThemeData(
+    primarySwatch: Colors.amber,
+    // 미정
+  );
+}
