9daf2ce8e14d8c970f2e6cf827196b282d5cc28b
hodu <kinnylovemy@gmail.com>
2023-07-02 16:50:56 +0900

    fix: kotlin 오류 수정

--- android/app/src/main/kotlin/com/example/my_tiny_map/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_tiny_map/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.my_tiny_map
 
+import io.flutter.embedding.android.FlutterActivity
 
-import android.os.Bundle
-
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        intent.putExtra("background_mode", "transparent")
-        super.onCreate(savedInstanceState)
-    }
+class MainActivity: FlutterActivity() {
 }
