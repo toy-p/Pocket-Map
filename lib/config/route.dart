f79acef89574cb24f4c3dcc0918a454e1bb23520
okm1172 <okm1172@naver.com>
2023-07-02 18:13:26 +0900

    image크기 조정, 저장 버튼 press시 dialog표시, Navigator경로 수정

--- lib/config/route.dart
+++ lib/config/route.dart
@@ -1,15 +1,12 @@
 import 'package:my_tiny_map/pages/home_page.dart';
 import 'package:my_tiny_map/screens/add_location_screen.dart';
-import 'package:my_tiny_map/utils/show_all_memory.dart';
 
 class RouteName {
   static const home = '/';
   static const addLocation = '/addLocation';
-  static const customBottomSheet = '/customBottomSheet';
 }
 
 var nameRoutes = {
   RouteName.home: (context) => const HomePage(),
   RouteName.addLocation: (context) => const AddLocationScreen(),
-  RouteName.customBottomSheet: (context) => const CustomBottomSheet(),
 };
