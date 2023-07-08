a86425f56c8caeac9d94af0d1ba2fd16bed27bf8
baka9131 <baka9131@gmail.com>
2023-07-08 20:13:03 +0900

    카카오맵 기능 추가 및 위치정보, 도로명 주소 검색(검색후 위치이동) 기능 추가 완료

--- lib/datas/models/image_model.dart
+++
@@ -1,6 +0,0 @@
-class ImgItemModel {
-  ImgItemModel(this.image, this.name);
-
-  final String image;
-  final String name;
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:my_tiny_map/config/route.dart';
 import 'package:my_tiny_map/design/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NaverMapSdk.instance.initialize(
-    clientId: 'fw9q4cz6x4',
-    onAuthFailed: (error) {
-      print('Auth failed : $error');
-    },
-  );
+  AuthRepository.initialize(appKey: 'f4f4541d62360e83a055df52209d4e2a');
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
