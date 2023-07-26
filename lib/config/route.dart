6c76493842a392dd9239e49917ed00bcf110e44e
LHJ <95395563+brownglasses@users.noreply.github.com>
2023-07-26 13:27:31 +0900

    디버깅 오류 수정과 파일 이미지 업로드를 구현하였습니다 (#22)

--- lib/config/route.dart
+++ lib/config/route.dart
@@ -1,15 +1,12 @@
 import 'package:my_tiny_map/pages/home_page.dart';
 import 'package:my_tiny_map/screens/add_location_screen.dart';
-import 'package:my_tiny_map/screens/select_Image.dart';
 
 class RouteName {
   static const home = '/';
   static const addLocation = '/addLocation';
-  static const selectImage = '/selectImage';
 }
 
 var nameRoutes = {
   RouteName.home: (context) => const HomePage(),
   RouteName.addLocation: (context) => const AddLocationScreen(),
-  RouteName.selectImage: (context) => const SelectImageScreen(),
 };
