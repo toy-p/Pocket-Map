ef6f10403365b842849770d5cb6a5bca8a7a6b81
brownglasses <lhj1583@naver.com>
2023-07-31 00:42:33 +0900

    fix/add_marker_ui

---
+++ android/app/src/main/kotlin/com/example/my_tiny_map/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pocket_m
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
+}
